d0d69a81d3a750f8326548bc4acaff50b485b6f6
Rob Habraken <roberthabraken@hotmail.com>
2020-12-11 16:45:28 +0100

    Add controller nodeselector to ensure ingress installation on Linux pool instead of Win pool, and add wait parameter to make the script synchronous (required for the future YAML pipeline so the script only continues when the Ingress controller is fully deployed)

--- azure/scripts/deploy-ingress-controller.ps1
+++ azure/scripts/deploy-ingress-controller.ps1
@@ -10,7 +10,9 @@ helm upgrade --install nginx-ingress ingress-nginx/ingress-nginx `
     --set controller.replicaCount=2 `
     --set controller.nodeSelector."beta\.kubernetes\.io/os"=linux `
     --set defaultBackend.nodeSelector."beta\.kubernetes\.io/os"=linux `
+    --set controller.admissionWebhooks.patch.nodeSelector."beta\.kubernetes\.io/os"=linux `
     --set-string controller.config.proxy-body-size=10m `
-    --set controller.service.externalTrafficPolicy=Local
+    --set controller.service.externalTrafficPolicy=Local `
+    --wait
 
 Write-Host "--- Ready setting up nginx, now retrieving DNS data... ---" -ForegroundColor Green
